4627be1e0900eeecceb92b78ce72f4c2ae495c61
wilson <ex_hewx@partner.midea.com>
2020-06-01 16:36:14 +0800

    删除响应码

--- src/main/java/io/springframework/common/response/ResponseConstant.java
+++ src/main/java/io/springframework/common/response/ResponseConstant.java
@@ -6,15 +6,43 @@ package io.springframework.common.response;
  **/
 public interface ResponseConstant {
     int SUCCESS = 200;
-    int PARAM_ERROR = 400;
+    /**
+     * 参数错误
+     */
+    int BAD_REQUEST = 400;
+    /**
+     * 未登录
+     */
     int UN_AUTHENTICATION = 401;
-    int ACCESS_DENY = 403;
+    /**
+     * 权限不足
+     */
+    int FORBIDDEN = 403;
     int NOT_FOUND = 404;
+    /**
+     * 请求方法错误(POST、GET、PUT、DELETE)
+     */
     int METHOD_NOT_ALLOWED = 405;
-    int UN_AUTHORIZATION = 406;
+    /**
+     * 无法使用请求的内容特性响应请求的网页
+     */
+    int NOT_ACCEPTABLE = 406;
+    /**
+     * 请求超时
+     */
     int REQUEST_TIMEOUT = 408;
+    /**
+     * 不支持的Content-Type {@link org.springframework.http.MediaType}
+     */
+    int UNSUPPORTED_MEDIA_TYPE = 415;
+    /**
+     * 服务器内部错误
+     */
     int SERVER_ERROR = 500;
     int BAD_GATEWAY = 502;
+    /**
+     * gateway超时
+     */
     int GATEWAY_TIMEOUT = 504;
 
     String SUCCESS_MSG = "success";
