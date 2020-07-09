5ff04de7e804d375732a61ec4364ad8dc248817b
Wilson-He <ex_hewx@partner.midea.com>
2020-07-09 18:29:16 +0800

    move default response to ServerResponse

--- src/main/java/io/springframework/common/response/ServerResponse.java
+++ src/main/java/io/springframework/common/response/ServerResponse.java
@@ -1,21 +1,31 @@
 package io.springframework.common.response;
 
-import lombok.Getter;
 import lombok.ToString;
 
 /**
  * @author Wilson
  * @date 2019/9/20
  **/
-@Getter
 @ToString
 public class ServerResponse<T> {
     private Integer code;
     private String msg;
     private T data;
 
+    private static final ServerResponse SUCCESS = ServerResponse.build(DefaultCodeMsg.SUCCESS);
+    private static final ServerResponse PARAM_FAILED = ServerResponse.build(DefaultCodeMsg.PARAM_FAILED);
+    private static final ServerResponse UN_AUTHENTICATION = ServerResponse.build(DefaultCodeMsg.UN_AUTHENTICATION);
+    private static final ServerResponse ACCESS_DENY = ServerResponse.build(DefaultCodeMsg.ACCESS_DENY);
+    private static final ServerResponse NOT_FOUND = ServerResponse.build(DefaultCodeMsg.NOT_FOUND);
+    private static final ServerResponse METHOD_NOT_ALLOWED = ServerResponse.build(DefaultCodeMsg.METHOD_NOT_ALLOWED);
+    private static final ServerResponse UN_AUTHORIZATION = ServerResponse.build(DefaultCodeMsg.UN_AUTHORIZATION);
+    private static final ServerResponse REQUEST_TIMEOUT = ServerResponse.build(DefaultCodeMsg.REQUEST_TIMEOUT);
+    private static final ServerResponse SERVER_ERROR = ServerResponse.build(DefaultCodeMsg.SERVER_ERROR);
+    private static final ServerResponse BAD_GATEWAY = ServerResponse.build(DefaultCodeMsg.BAD_GATEWAY);
+    private static final ServerResponse GATEWAY_TIMEOUT = ServerResponse.build(DefaultCodeMsg.GATEWAY_TIMEOUT);
+
     public static ServerResponse success() {
-        return DefaultServerResponse.SUCCESS;
+        return SUCCESS;
     }
 
     public static <T> ServerResponse<T> success(T data) {
@@ -33,12 +43,30 @@ public class ServerResponse<T> {
     /**
      * 服务器内部错误
      *
+     * @return
+     */
+    public static ServerResponse serverError() {
+        return SERVER_ERROR;
+    }
+
+    /**
+     * 服务器内部错误
+     *
      * @param msg 错误信息
      * @param <T>
      * @return
      */
     public static <T> ServerResponse<T> serverError(String msg) {
-        return new ServerResponse<>(DefaultCodeMsg.SERVER_ERROR.code(), msg);
+        return new ServerResponse<>(SERVER_ERROR.code, msg);
+    }
+
+    /**
+     * 参数错误
+     *
+     * @return
+     */
+    public static ServerResponse paramError() {
+        return PARAM_FAILED;
     }
 
     /**
@@ -48,7 +76,7 @@ public class ServerResponse<T> {
      * @return
      */
     public static <T> ServerResponse<T> paramError(String msg) {
-        return new ServerResponse<>(DefaultCodeMsg.PARAM_FAILED.code(), msg);
+        return new ServerResponse<>(PARAM_FAILED.code, msg);
     }
 
     private ServerResponse(ResponseCodeMsg codeMsg) {
@@ -70,5 +98,53 @@ public class ServerResponse<T> {
     public ServerResponse<T> data(T data) {
         this.data = data;
         return this;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public static ServerResponse paramFailed() {
+        return PARAM_FAILED;
+    }
+
+    public static ServerResponse unAuthentication() {
+        return UN_AUTHENTICATION;
+    }
+
+    public static ServerResponse accessDeny() {
+        return ACCESS_DENY;
+    }
+
+    public static ServerResponse notFound() {
+        return NOT_FOUND;
+    }
+
+    public static ServerResponse methodNotAllowed() {
+        return METHOD_NOT_ALLOWED;
+    }
+
+    public static ServerResponse unAuthorization() {
+        return UN_AUTHORIZATION;
+    }
+
+    public static ServerResponse requestTimeout() {
+        return REQUEST_TIMEOUT;
+    }
+
+    public static ServerResponse badGateway() {
+        return BAD_GATEWAY;
+    }
+
+    public static ServerResponse gatewayTimeout() {
+        return GATEWAY_TIMEOUT;
     }
 }
